1e45ac1f4d4f9d5a9f7d7443d5aac26d5197a44e
Pedro Gallardo <pegallardo@inf.udec.cl>
2023-06-29 16:29:42 -0400

    feat: added pyroscope to start step

--- correction/_step/__init__.py
+++ correction/_step/__init__.py
@@ -1,5 +1,14 @@
 from .step import CorrectionStep
+import os
+import pyroscope
 
 
 def run_step():
-    CorrectionStep.create_step().start()
+    if bool(os.getenv("USE_PROFILING", True)):
+        pyroscope.configure(application_name="steps.correction", server_address=os.getenv("PYROSCOPE_SERVER"))
+
+        with pyroscope.tag_wrapper({"function": "start"}):
+            CorrectionStep.create_step().start()
+
+    else:
+        CorrectionStep.create_step().start()
